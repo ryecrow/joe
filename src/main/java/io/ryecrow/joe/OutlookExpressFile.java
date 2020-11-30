c70dcac03f1b34765e9149135788f5630b13f1b9
ryecrow <renefang@outlook.com>
2020-11-30 16:20:38 +0800

    Check File Header when loading DBX file

--- src/main/java/io/ryecrow/joe/OutlookExpressFile.java
+++ src/main/java/io/ryecrow/joe/OutlookExpressFile.java
@@ -1,5 +1,7 @@
 package io.ryecrow.joe;
 
+import io.ryecrow.joe.internal.DatabaseType;
+
 import java.io.Closeable;
 import java.io.File;
 import java.io.IOException;
@@ -11,7 +13,12 @@ public class OutlookExpressFile implements Closeable {
      * The content of the DBX file,
      * presented in the form of a {@link RandomAccessFile}
      */
-    private RandomAccessFile dbxFile;
+    private final RandomAccessFile dbxFile;
+
+    /**
+     *
+     */
+    private DatabaseType dbType;
 
     public OutlookExpressFile(File file) throws IOException {
         this(new RandomAccessFile(file, "r"));
@@ -28,6 +35,7 @@ public class OutlookExpressFile implements Closeable {
      */
     private OutlookExpressFile(RandomAccessFile file) throws IOException {
         this.dbxFile = file;
+        checkFile();
     }
 
     /**
@@ -42,5 +50,23 @@ public class OutlookExpressFile implements Closeable {
         if (this.dbxFile != null) {
             this.dbxFile.close();
         }
+    }
+
+    private void checkFile() throws IOException {
+        int magic = this.dbxFile.readInt();
+        if (magic != 0xcfad12fe) {
+            throw new IOException("Invalid file header. Expected: 0xcfad12fe. Actual: 0x" + Integer.toHexString(magic));
+        }
+        int ft = this.dbxFile.readInt();
+        DatabaseType dbType = DatabaseType.getByUid(ft);
+        if (dbType == null) {
+            throw new IOException("Invalid file type: 0x" + Integer.toHexString(ft));
+        }
+        this.dbType = dbType;
+
+        if ((dbxFile.readLong() != 0x66e3d1119a4e00c0L) && (dbxFile.readLong() != 0x4fa309d405000000L)) {
+            throw new IOException("Invalid DBX file format");
+        }
+        this.dbxFile.seek(0);
     }
 }
